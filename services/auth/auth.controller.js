18236f3d845f23e9ae378ff90c1f8de7d6f119d9
Luis Angel <luisparr14@gmail.com>
2023-04-03 19:52:07 -0500

    clase #80 - flutter avanzado

--- services/auth/auth.controller.js
+++ services/auth/auth.controller.js
@@ -1,14 +1,34 @@
 const User = require('../../models/user.model');
 
 const Register = async (req, res) => {
+  try {
 
-  await User.create(req.body);
+    const { email } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: 'User created',
-    user: req.body,
-  });
+    const emailExists = User.findOne({ email });
+
+    if (emailExists) {
+      return res.status(400).send({
+        ok: false,
+        message: 'An error occurred while creating the user',
+      });
+    }
+
+    const user = new User(req.body);
+    await user.save();
+
+    res.status(201).send({
+      ok: true,
+      message: 'User created successfully',
+      user,
+    });
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      message: 'Error creating user',
+      error,
+    });
+  }
 };
 
 module.exports = {
