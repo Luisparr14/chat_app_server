2dbb5aa7fbdd61217d62858be0b3145952fa91c1
Luis Angel <luisparr14@gmail.com>
2023-04-07 23:23:46 -0500

    clase #114 - flutter avanzado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,21 +1,28 @@
 const { connectedUser, disconnectedUser } = require("../services/sockets/sockets.controller")
 const { verifyJWT } = require("../utils/jwt")
 
-const socket = (io = io) => {
+const socket = (io) => {
   io.on('connection', (client) => {
     console.log(`Client connected...`)
     const token = client.handshake.headers['x-token']
     const { uid, valid } = verifyJWT(token, process.env.JWT_SECRET)
 
     if (!valid) return client.disconnect()
+    client.join(uid)
     console.log(`Client authenticated...`, uid)
     connectedUser(uid)
     
+    client.on('personal-message', (payload) => {
+      console.log('Personal message', payload)
+      io.to(payload.to).emit('personal-message', payload)
+    })
+
     client.on('disconnect', () => {
       console.log(`Client disconnected...`)
       disconnectedUser(uid)
     })
   })
+
 }
 
 module.exports = socket
